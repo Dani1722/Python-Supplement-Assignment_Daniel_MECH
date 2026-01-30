81f3364a86ea93ec5ddd7b5197ec3fcf25cb8508
Dani1722 <adanielamalraj@gmail.com>
2026-01-30 16:27:17 +0530

    new changes

--- problem_14.py
+++ problem_14.py
@@ -2,9 +2,9 @@
 # Find and fix the error
 
 def is_prime(n):
-    if n < 2:
+    if n <2:
         return False
-    for i in range(2, n):
+    for i in range(2, n-1):
         if n % i == 0:
             return False
     return True
